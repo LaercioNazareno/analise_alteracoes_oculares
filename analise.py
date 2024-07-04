82504819abf35c3c467a1c45bf15ac5d87fa968c
Laercio <laercio.nazareno1@gmail.com>
2024-07-03 23:02:12 -0300

    correlacao

--- analise.py
+++ analise.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import kendalltau
+
 class Analise:
     
     def __init__(self) -> None:
@@ -8,8 +10,8 @@ class Analise:
     def buscar_dados_por(self, campo, valor):
         return self.df_animais[self.df_animais[campo] == valor]
 
-    def agrupar_df_por(self, df:pd.DataFrame, campo: str):
-        df_agrupado = df.groupby([campo]).size().reset_index(name='Quantidade')
+    def agrupar_df_por(self, df:pd.DataFrame, campo):
+        df_agrupado = df.groupby(campo).size().reset_index(name='Quantidade')
         total_count = df_agrupado['Quantidade'].sum()
         df_agrupado['Porcentagem'] = ((df_agrupado['Quantidade'] / total_count) * 100).round(2)
         return df_agrupado
@@ -39,4 +41,23 @@ class Analise:
         df2 = pd.read_csv('./dados/dados_animais_alteracoes.csv')
         df_merged = pd.merge(df, df2, on='Animal', how='inner')  
         return df_merged[['alteracao','tem_leishmaniose']]
+    
+    def buscar_colunas(self):
+        df2 = pd.read_csv('./dados/dados_animais_alteracoes.csv')
+        df_merged = pd.merge(self.df_animais, df2, on='Animal', how='inner')
+        return df_merged.columns
+         
+    
+    def buscar_correlacao(self, colum):
+        df2 = pd.read_csv('./dados/dados_animais_alteracoes.csv')
+        df_merged = pd.merge(self.df_animais, df2, on='Animal', how='inner') 
         
+        correlacoes = []
+        coluns = df_merged.columns
+        campo_1 = df_merged[colum]
+        for colum_2 in coluns:
+            campo_2 = df_merged[colum_2]
+            if colum != colum_2:
+                corr, p_value = kendalltau(campo_1, campo_2)
+                correlacoes.append({"coluna_1": colum, "coluna_2": colum_2, "correlcao":corr, "p_valor": p_value})
+        return pd.DataFrame(correlacoes)
--- app.py
+++ app.py
@@ -115,7 +115,7 @@ st.write(analise_txt)
 st.subheader('DIAGNÓSTICOS')
 
 diagnosticos_distintos = analise.buscar_valores_distintos(df, 'DIAGNÓSTICO')
-df_agrupado = analise.agrupar_df_por(df, 'DIAGNÓSTICO')
+df_agrupado = analise.agrupar_df_por(df, ['DIAGNÓSTICO', 'LOCALIZAÇÃO'])
 
 analise_txt = f'''
     A base de dados contém {len(diagnosticos_distintos)} direfentes tipos DIAGNÓSTICOS que são distribuidos daseguinte maneira:\n
@@ -135,7 +135,7 @@ st.altair_chart(bar_chart, use_container_width=True)
 st.subheader('DIAGNÓSTICOS')
 
 diagnosticos_distintos = analise.buscar_valores_distintos(df, 'DIAGNÓSTICO')
-df_agrupado = analise.agrupar_df_por(df, 'DIAGNÓSTICO')
+df_agrupado = analise.agrupar_df_por(df, ['DIAGNÓSTICO', 'LOCALIZAÇÃO'])
 
 analise_txt = f'''
     A base de dados contém {len(diagnosticos_distintos)} direfentes tipos DIAGNÓSTICOS que são distribuidos daseguinte maneira:\n
@@ -278,27 +278,7 @@ st.write(analise_txt)
 st.subheader('DIAGNÓSTICOS')
 
 diagnosticos_distintos = analise.buscar_valores_distintos(df, 'DIAGNÓSTICO')
-df_agrupado = analise.agrupar_df_por(df, 'DIAGNÓSTICO')
-
-analise_txt = f'''
-    A base de dados contém {len(diagnosticos_distintos)} direfentes tipos DIAGNÓSTICOS que são distribuidos daseguinte maneira:\n
-'''
-st.write(analise_txt)
-
-st.dataframe(df_agrupado)
-bar_chart = alt.Chart(df_agrupado).mark_bar().encode(
-    x=alt.X('Quantidade', title='Quantidade'),
-    y=alt.Y('DIAGNÓSTICO', title='DIAGNÓSTICO', sort='-x'),
-    color='DIAGNÓSTICO'
-).properties(
-    title=f'Quantidade de DIAGNÓSTICO'
-)
-st.altair_chart(bar_chart, use_container_width=True)
-########################################################################################################################
-st.subheader('DIAGNÓSTICOS')
-
-diagnosticos_distintos = analise.buscar_valores_distintos(df, 'DIAGNÓSTICO')
-df_agrupado = analise.agrupar_df_por(df, 'DIAGNÓSTICO')
+df_agrupado = analise.agrupar_df_por(df, ['DIAGNÓSTICO', 'LOCALIZAÇÃO'])
 
 analise_txt = f'''
     A base de dados contém {len(diagnosticos_distintos)} direfentes tipos DIAGNÓSTICOS que são distribuidos daseguinte maneira:\n
@@ -335,3 +315,154 @@ bar_chart = alt.Chart(df_agrupado).mark_bar().encode(
     title=f'Quantidade de alterações'
 )
 st.altair_chart(bar_chart, use_container_width=True)
+########################################################################################################################
+st.header('Análise dos animais com leishmaniose')
+########################################################################################################################
+
+df = analise.buscar_dados_por(campo='tem_leishmaniose', valor=1)
+qtd_total = analise.calcular_qtd(df)
+df_agrupado = analise.agrupar_df_por(df, 'RAÇA')
+
+analise_txt = f'''
+    A base de dados contém {qtd_total} avaliações de animais, distribuido da seguinte maneira:\n
+'''
+st.write(analise_txt)
+
+bar_chart = alt.Chart(df_agrupado).mark_bar().encode(
+    x=alt.X('Quantidade', title='Quantidade'),
+    y=alt.Y('RAÇA', title='RAÇA', sort='-x'),
+    color='RAÇA'
+).properties(
+    title=f'Quantidade de animais'
+)
+st.altair_chart(bar_chart, use_container_width=True)
+########################################################################################################################
+st.subheader('Idade')
+df_agrupado = analise.agrupar_df_por(df, 'IDADE')
+media = analise.calcular_media(df, 'IDADE')
+maxima = analise.calcular_maximo(df, 'IDADE')
+minima = analise.calcular_minimo(df, 'IDADE')
+dv = analise.calcular_std(df, 'IDADE')
+
+analise_txt = f'''
+    O animal mais novo tem {minima} anos, \n
+    o animal mais velho tem {maxima} anos,\n
+    a média de idade da populção é de {media} com um desvio padrão de {dv}
+'''
+st.write(analise_txt)
+########################################################################################################################
+st.subheader('TLS OD')
+media = analise.calcular_media(df, 'TLS OD')
+maxima = analise.calcular_maximo(df, 'TLS OD')
+minima = analise.calcular_minimo(df, 'TLS OD')
+dv = analise.calcular_std(df, 'TLS OD')
+
+analise_txt = f'''
+    O menor valor encontrado {minima}, \n
+    o maior valor encontrado {maxima},\n
+    a média de valores é {media} com um desvio padrão de {dv}
+'''
+st.write(analise_txt)
+########################################################################################################################
+st.subheader('TLS OE')
+media = analise.calcular_media(df, 'TLS OE')
+maxima = analise.calcular_maximo(df, 'TLS OE')
+minima = analise.calcular_minimo(df, 'TLS OE')
+dv = analise.calcular_std(df, 'TLS OE')
+
+analise_txt = f'''
+    O menor valor encontrado {minima}, \n
+    o maior valor encontrado {maxima},\n
+    a média de valores é {media} com um desvio padrão de {dv}
+'''
+st.write(analise_txt)
+########################################################################################################################
+st.subheader('PIO OD')
+media = analise.calcular_media(df, 'PIO OD')
+maxima = analise.calcular_maximo(df, 'PIO OD')
+minima = analise.calcular_minimo(df, 'PIO OD')
+dv = analise.calcular_std(df, 'PIO OD')
+
+analise_txt = f'''
+    O menor valor encontrado {minima}, \n
+    o maior valor encontrado {maxima},\n
+    a média de valores é {media} com um desvio padrão de {dv}
+'''
+st.write(analise_txt)
+########################################################################################################################
+st.subheader('PIO OE')
+media = analise.calcular_media(df, 'PIO OE')
+maxima = analise.calcular_maximo(df, 'PIO OE')
+minima = analise.calcular_minimo(df, 'PIO OE')
+dv = analise.calcular_std(df, 'PIO OE')
+
+analise_txt = f'''
+    O menor valor encontrado {minima}, \n
+    o maior valor encontrado {maxima},\n
+    a média de valores é {media} com um desvio padrão de {dv}
+'''
+st.write(analise_txt)
+########################################################################################################################
+st.subheader('Teste de fluorosceína')
+qtd_positivo = analise.count_valores_igual(df, 'Teste de fluorosceína', 1)
+qtd_negativo = analise.count_valores_igual(df, 'Teste de fluorosceína', 0)
+analise_txt = f'''
+    valores positivos:\n
+    \tquantidade: {qtd_positivo}
+    \tporcentagem: {round((qtd_positivo/qtd_total)*100, 2)} %
+    \n
+    valores negativos:\n
+    \tquantidade: {qtd_negativo}
+    \tporcentagem: {round((qtd_negativo/qtd_total)*100, 2)} %
+    \n
+'''
+st.write(analise_txt)
+########################################################################################################################
+st.subheader('DIAGNÓSTICOS')
+
+diagnosticos_distintos = analise.buscar_valores_distintos(df, 'DIAGNÓSTICO')
+df_agrupado = analise.agrupar_df_por(df, ['DIAGNÓSTICO', 'LOCALIZAÇÃO'])
+
+analise_txt = f'''
+    A base de dados contém {len(diagnosticos_distintos)} direfentes tipos DIAGNÓSTICOS que são distribuidos daseguinte maneira:\n
+'''
+st.write(analise_txt)
+
+st.dataframe(df_agrupado)
+bar_chart = alt.Chart(df_agrupado).mark_bar().encode(
+    x=alt.X('Quantidade', title='Quantidade'),
+    y=alt.Y('DIAGNÓSTICO', title='DIAGNÓSTICO', sort='-x'),
+    color='DIAGNÓSTICO'
+).properties(
+    title=f'Quantidade de DIAGNÓSTICO'
+)
+st.altair_chart(bar_chart, use_container_width=True)
+########################################################################################################################
+st.subheader('Alterações')
+
+df = analise.buscar_dataframe_alteracao(df)
+diagnosticos_distintos = analise.buscar_valores_distintos(df, 'alteracao')
+df_agrupado = analise.agrupar_df_por(df, 'alteracao')
+
+analise_txt = f'''
+    A base de dados contém {len(diagnosticos_distintos)} direfentes tipos alterações que são distribuidos daseguinte maneira:\n
+'''
+st.write(analise_txt)
+
+st.dataframe(df_agrupado)
+bar_chart = alt.Chart(df_agrupado).mark_bar().encode(
+    x=alt.X('Quantidade', title='Quantidade'),
+    y=alt.Y('alteracao', title='alterações', sort='-x'),
+    color='alteracao'
+).properties(
+    title=f'Quantidade de alterações'
+)
+st.altair_chart(bar_chart, use_container_width=True)
+
+########################################################################################################################
+st.header('Correlação')
+########################################################################################################################
+
+campo = st.selectbox("Selecione um campo", analise.buscar_colunas())
+dados = analise.buscar_correlacao([campo])
+st.dataframe(dados)
